7b61ff987549b73b2bfbca8597e1e9bd2a3800cd
Olha Mykhailyk <omykhailyk@knu.ua>
2025-02-18 13:14:13 +0200

    added new files

---
+++ main.py
@@ -0,0 +1,6 @@
+from utils import *
+
+if __name__ == "__main__":
+    print(find_factorial(6))
+    print(gcd(56, 98))
+    print(is_prime(121))
---
+++ utils.py
@@ -0,0 +1,31 @@
+def is_power_of_five(n):
+    if n <= 0:
+        return False
+    while n % 5 == 0:
+        n //= 5
+    return n == 1
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def find_factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+def gcd(a, b):
+    while b:
+        a,b = b, a%b
+    return a
+
+if __name__ == "__main__":
+    print(find_factorial(5))
+
+
